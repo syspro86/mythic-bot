20024f8c3c3268ce820a70cc3fb4237e89b70170
root <root@code.nas.zsoo.net>
2020-11-16 00:18:56 +0900

    탈것 수집

--- mythic/pet/collect.py
+++ mythic/pet/collect.py
@@ -36,25 +36,65 @@ class CollectPetBot(object):
 
         pets = pets['pets']
         for pet in pets:
+            changed = True
             _id = pet['id']
             pet_saved = self.db.find_pet(_id)
             if pet_saved is not None:
                 pet = pet_saved
+                changed = False
 
             if 'key' in pet:
                 href = pet['key']['href']
                 pet_detail = self.api.bn_request(href, token=True)
                 if pet_detail is not None:
                     pet = pet_detail
+                    changed = True
 
             if 'media' in pet and 'key' in pet['media']:
                 media_href = pet['media']['key']['href']
                 pet_media = self.api.bn_request(media_href, token=True)
                 if pet_media is not None:
                     pet['media'] = pet_media
+                    changed = True
 
-            pet['_id'] = _id
-            self.db.insert_pet(pet, upsert=True)
+            if changed:
+                pet['_id'] = _id
+                self.db.insert_pet(pet, upsert=True)
+
+    def update_mount_index(self):
+        mounts = self.api.bn_request(F"/data/wow/mount/index", token=True, namespace="static")
+        if mounts is None or 'mounts' not in mounts:
+            return
+
+        mounts = mounts['mounts']
+        for mount in mounts:
+            changed = True
+            _id = mount['id']
+            mount_saved = self.db.find_mount(_id)
+            if mount_saved is not None:
+                mount = mount_saved
+                changed = False
+
+            if 'key' in mount:
+                href = mount['key']['href']
+                mount_detail = self.api.bn_request(href, token=True)
+                if mount_detail is not None:
+                    mount = mount_detail
+                    changed = True
+
+            if 'creature_displays' in mount:
+                creature_displays = mount['creature_displays']
+                for i in range(len(creature_displays)):
+                    if 'key' in creature_displays[i]:
+                        cd_href = creature_displays[i]['key']['href']
+                        mount_media = self.api.bn_request(cd_href, token=True)
+                        if mount_media is not None:
+                            creature_displays[i] = mount_media
+                            changed = True
+
+            if changed:
+                mount['_id'] = _id
+                self.db.insert_mount(mount, upsert=True)
 
     def update_trainer(self, realm, character_name):
         pets = self.api.bn_request(f"/profile/wow/character/{realm}/{character_name}/collections/pets", token=True, namespace="profile")
@@ -78,8 +118,9 @@ class CollectPetBot(object):
         try:
             now_ts = int(datetime.now().timestamp() * 1000)
 
+            self.update_mount_index()
             self.update_pet_index()
-            self.update_trainer('hyjal', '터널기사')
+            #self.update_trainer('hyjal', '터널기사')
 
             now_ts2 = int(datetime.now().timestamp() * 1000)
             logger.info(f'collected in {now_ts2 - now_ts} ms')
--- mythic/pet/db.py
+++ mythic/pet/db.py
@@ -27,6 +27,27 @@ class PetDatabase:
 
         return self.db.pets.find_one({'_id': id})
 
+    def insert_mount(self, record, upsert=False):
+        if self.db is None:
+            logger.info(record)
+            return True
+
+        try:
+            if upsert:
+                self.db.mounts.replace_one({'_id': record['_id']}, record, upsert=True)
+            else:
+                self.db.mounts.insert_one(record)
+            return True
+        except pymongo.errors.DuplicateKeyError:
+            return False
+
+    def find_mount(self, id):
+        if self.db is None:
+            logger.info(id)
+            return True
+
+        return self.db.mounts.find_one({'_id': id})
+
     def insert_trainer(self, record, upsert=False):
         if self.db is None:
             logger.info(record)
