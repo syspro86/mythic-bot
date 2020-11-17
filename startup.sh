e04424162e1fddc4fb01b0bb331f14b42ec6f225
root <root@code.nas.zsoo.net>
2020-11-17 22:31:09 +0900

    $@

--- startup.sh
+++ startup.sh
@@ -2,7 +2,7 @@
 
 if [ -f /app/appd.cfg ]; then
   pip install -U appdynamics || exit $?
-  pyagent run -c /app/appd.cfg python -u -m mythic.main $*
+  pyagent run -c /app/appd.cfg python -u -m mythic.main $@
 else
-  python -u -m mythic.main $*
+  python -u -m mythic.main $@
 fi
